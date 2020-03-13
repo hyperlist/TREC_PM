55e1933329584ada6ec9a51f75b05ddd411db44d
hyperlist <hyperlist@1427589623@qq.com>
2020-03-13 16:10:29 +0800

    debug

--- query_elastic.py
+++ query_elastic.py
@@ -2,7 +2,9 @@ import elasticsearch
 import json,re,time,sys
 from data import DataManager
     
-def construct_ct_query(extracted_data, name):
+template = 'BASE.json'
+
+def construct_ct_query(extracted_data):
     disease = extracted_data['disease']
     gene = extracted_data['gene']
     age = extracted_data['age']
@@ -15,17 +17,16 @@ def construct_ct_query(extracted_data, name):
     geneDescriptions = extracted_data['geneDescriptions']
     
     #获取查询模板
+    temp = DataManager.get_template(template)
+    
     seq = " "
-    temp = DataManager.get_template(name)
-    temp = temp.replace('"{{age}}"',str(age))
+    temp = temp.replace('{{age}}',str(age))
     temp = temp.replace('{{gene}}',gene)
     temp = temp.replace('{{disease}}',disease)
     temp = temp.replace('{{sex}}',sex)
     temp = temp.replace('{{other}}',str(other))
-    
     temp = temp.replace('{{diseasePreferredTerm}}', str(diseasePreferredTerm))
     temp = temp.replace('{{[geneDescriptions]}}',str(geneDescriptions))
-    
     temp = temp.replace('{{[diseaseSynonyms]}}',seq.join(diseaseSynonyms))
     temp = temp.replace('{{[diseaseHypernyms]}}',seq.join(diseaseHypernyms))
     #temp = temp.replace('{{[customDiseaseExpansions]}}',str(diseaseSynonyms))     #customDiseaseExpansions
@@ -39,21 +40,21 @@ def construct_ct_query(extracted_data, name):
     return json.loads(temp)
     
 #curl -XGET 'http://localhost:9200/ct/xml/_validate/query?explain' -H 'Content-Type: application/json' -d @test.txt
-def ct_query(extracted_data,template):
-    query = construct_ct_query(extracted_data, template)
+def ct_query(extracted_data):
+    query = construct_ct_query(extracted_data)
     #print(query)
     r = es.search(index='ct', body=query, size=500,request_timeout=120)
     print('total is : ', r['hits']["total"], 'ac number is ', len(r['hits']['hits']))
     return r['hits']['hits']
         
-def save_ct_result(template):
+def save_ct_result():
     topics = DataManager.extract_query_extension()
     op_file = open('qresults/ct_results.txt', 'w')
     for item in topics:
         rank_ctr = 1
         print('query topic: ',item['tnum'], ' disease: ', item['disease'])
         starttime = time.time()
-        res = ct_query(item,template)
+        res = ct_query(item)
         for i in res:
             op_file.write('{}\tQ0\t{}\t{}\t{}\tbaseline\n'.format(item['tnum'], i['_source']['nct_id'], rank_ctr, round(i['_score'], 4)))
             rank_ctr += 1
@@ -75,7 +76,6 @@ def intersection_query():
     
 if __name__ == '__main__':
     
-    template = 'BASE.json'
     
     try:
         es = elasticsearch.Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -83,6 +83,6 @@ if __name__ == '__main__':
         print('Error Message:', e, '\n')
         raise Exception("\nCannot connect to Elasticsearch!")
     # Call the function to start extracting the queries
-    save_ct_result(template)
+    save_ct_result()
     intersection_query()
     
